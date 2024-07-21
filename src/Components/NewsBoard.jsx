21d1268bfc1ad402884668b467b4596484c51307
Sanidhya Vijay <messi.god1029@gmail.com>
2024-07-22 02:55:50 +0530

    env transfer

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({category}) => {
   const [articles,setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=96899aac1cd240ae8e8d54d4ba386a73`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
     fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
   },[category])
