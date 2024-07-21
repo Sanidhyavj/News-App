720a1d07f8f67d5d9d8fa54e1b4162c7eb988044
Sanidhya Vijay <messi.god1029@gmail.com>
2024-07-22 03:16:34 +0530

    api change

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({category}) => {
   const [articles,setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url = `https://gnews.io/api/v4/top-headlines?category=${category}&country=us&max=10&apikey=${import.meta.env.VITE_API_KEY}`;
     fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
   },[category])
@@ -15,7 +15,7 @@ const NewsBoard = ({category}) => {
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
       {articles?.map((news,index)=>{
-        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.image} url={news.url} />
       })}
     </div>
   )
